3540a25f5df31b2f2821293849d2dcac9f637dd8
Simon Berger <simon@siku2.io>
2024-08-02 21:16:34 +0000

    just use vscode group

--- .devcontainer/on-create-root.sh
+++ .devcontainer/on-create-root.sh
@@ -5,6 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y pipx python3
 
-usermod -a -G dialout vscode
-chown :dialout /dev/tty*
+chown :vscode /dev/tty*
 chmod g+rw /dev/tty*
