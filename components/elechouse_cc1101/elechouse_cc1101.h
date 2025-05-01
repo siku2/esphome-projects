48fefc91649f932415ef95241407fdeeb4570d20
Simon Berger <simon@siku2.io>
2025-05-01 09:21:33 +0000

    fix namespace

--- components/elechouse_cc1101/elechouse_cc1101.h
+++ components/elechouse_cc1101/elechouse_cc1101.h
@@ -6,18 +6,18 @@ namespace siku2
 {
     namespace elechouse_cc1101
     {
-        class ElechouseCc1101 : public Component
+        class ElechouseCc1101 : public esphome::Component
         {
         public:
-            void set_tx_pin(InternalGPIOPin *tx_pin) { this->tx_pin_ = tx_pin; }
-            void set_rx_pin(InternalGPIOPin *rx_pin) { this->rx_pin_ = rx_pin; }
+            void set_tx_pin(esphome::InternalGPIOPin *tx_pin) { this->tx_pin_ = tx_pin; }
+            void set_rx_pin(esphome::InternalGPIOPin *rx_pin) { this->rx_pin_ = rx_pin; }
             void set_frequency(float frequency) { this->frequency_ = frequency; }
 
             void setup() override;
 
         protected:
-            InternalGPIOPin *tx_pin_;
-            InternalGPIOPin *rx_pin_;
+            esphome::InternalGPIOPin *tx_pin_;
+            esphome::InternalGPIOPin *rx_pin_;
             float frequency_{};
         };
     }
