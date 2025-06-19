e9c60812b34b1c09501d186308c608930ff5709a
Simon Berger <simon@siku2.io>
2025-06-19 16:44:11 +0000

    Update devcontainer

--- .devcontainer/on-create-root.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get install -y pipx python3
-
-chown :vscode /dev/tty*
-chmod g+rw /dev/tty*
--- .devcontainer/on-create.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo bash ./.devcontainer/on-create-root.sh
-
-pipx ensurepath
-pipx install esphome
